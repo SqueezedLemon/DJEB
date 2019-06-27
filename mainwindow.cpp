895d57c8e34664a6f86a29a07fea84df9aa03d50
Biraj Adhikari <52060444+SqueezedLemon@users.noreply.github.com>
2019-06-27 12:36:40 +0545

    Add files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,31 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMessageBox>
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString Username = ui->lineEdit_Username->text();
+    QString Password = ui->lineEdit_Password->text();
+
+    if (Username == "test" && Password == "test")
+    {
+        hide();
+        second = new Second(this);
+        second->show();
+    }
+    else
+    {
+        QMessageBox::warning(this,"Login","Username or Password is incorrect.");
+    }
+}
