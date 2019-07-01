ca8cd061920b6123e9317f2c81154d5d5dbcbddc
unknown <nabinghimire203@gmail.com>
2019-07-01 13:24:25 +0545

    Id updated

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -24,7 +24,7 @@ void MainWindow::on_pushButton_clicked()
     QString Username = ui->lineEdit_Username->text();
     QString Password = ui->lineEdit_Password->text();
 
-    if (Username == "test" && Password == "test" || Username == "crack" && Password == "crack")
+    if (Username == "test" && Password == "test" || Username == "cracks" && Password == "cracks")
     {
         hide();
         second = new Second(this);
